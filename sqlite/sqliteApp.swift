f662db54c2b2001db3696435e81709660fd0118d
Nickolay Trukhin <coolone.official@gmail.com>
2021-11-03 00:49:23 +0300

    Initial Commit

---
+++ sqlite/sqliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  sqliteApp.swift
+//  sqlite
+//
+//  Created by Nickolay Truhin on 03.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct sqliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
